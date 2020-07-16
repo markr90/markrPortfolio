dfe84eff2edc6d43043696ae9fb3f29631beca05
Mark <43848409+markr90@users.noreply.github.com>
2020-07-16 21:53:02 +0200

    Add project section

--- scripts.js
+++ scripts.js
@@ -11,3 +11,28 @@ $(document).click(function(e){
         dropdown.removeClass("show");
     }
 });
+
+$('a.homeLink').click(function(){
+    positionport = $('#headerSection').offset().top - $('.navbar').height();
+    $("html, body").animate({scrollTop:positionport}, '500', 'swing');
+})
+
+$('a.aboutMeLink').click(function(){
+    positionport = $('#aboutMeSection').offset().top - $('.navbar').height();
+    $("html, body").animate({scrollTop:positionport}, '500', 'swing');
+})
+
+$('a.projectsLink').click(function(){
+    positionport = $('#projectsSection').offset().top - $('.navbar').height();
+    $("html, body").animate({scrollTop:positionport}, '500', 'swing');
+})
+
+$('a.contactLink').click(function(){
+    positionport = $('#contactSection').offset().top - $('.navbar').height();
+    $("html, body").animate({scrollTop:positionport}, '500', 'swing');
+})
+
+$('a.skillsLink').click(function(){
+    positionport = $('#skillsSection').offset().top - $('.navbar').height();
+    $("html, body").animate({scrollTop:positionport}, '500', 'swing');
+})
